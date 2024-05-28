a51992b7d092b27c5131cf93a127ca2fe4ad2b48
arpitplexustechnology2024 <arpit.plexustechnology@gmail.com>
2024-05-27 18:00:42 -0700

    Commit

---
+++ UnduManager/MemberData.swift
@@ -0,0 +1,18 @@
+//
+//  MemberData.swift
+//  UnduManager
+//
+//  Created by Arpit iOS Dev. on 27/05/24.
+//
+
+import Foundation
+import UIKit
+
+struct MemberData {
+    
+    var name: String
+    var age: String
+    
+    static var arrEmployee : [MemberData] = [
+    ]
+}
